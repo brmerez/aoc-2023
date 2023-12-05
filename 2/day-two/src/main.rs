6fc7790f01c786063608c4937e9744678433cd2c
bruno.merez <bruno.merez@flashcourier.com.br>
2023-12-05 18:18:32 -0300

    Dia 2 -- parte 2

--- 2/day-two/src/main.rs
+++ 2/day-two/src/main.rs
@@ -36,6 +36,66 @@ impl Pair {
 }
 
 fn main() {
+    let games: Vec<Vec<Vec<Pair>>> = fs::read_to_string("input.txt")
+        .unwrap()
+        .lines()
+        .map(|x| parse_line(x.to_lowercase().as_str()))
+        .collect();
+
+    let mut minimum_colors_exp: usize = 0;
+    let mut correct_games_sum: usize = 0;
+
+    for (i, game) in games.iter().enumerate() {
+        println!("Game {}", i + 1);
+
+        // Part one of the challenge
+        if check_game_valid(&game) {
+            correct_games_sum += (i + 1);
+        }
+
+        // Part two
+        // finding the minimum number of each color
+        minimum_colors_exp += add_minimuns(&game);
+    }
+
+    println!("Sum of the ids of plausible games: {}", correct_games_sum);
+    println!(
+        "Sum of the powers of the min number of balls: {}",
+        minimum_colors_exp
+    );
+}
+
+fn add_minimuns(game: &Vec<Vec<Pair>>) -> usize {
+    let mut min_r = 0;
+    let mut min_b = 0;
+    let mut min_g = 0;
+
+    for set in game {
+        for pair in set {
+            match pair.color {
+                Colors::Red => {
+                    if pair.value > min_r {
+                        min_r = pair.value;
+                    }
+                }
+                Colors::Green => {
+                    if pair.value > min_g {
+                        min_g = pair.value;
+                    }
+                }
+                Colors::Blue => {
+                    if pair.value > min_b {
+                        min_b = pair.value;
+                    }
+                }
+            }
+        }
+    }
+
+    (min_b * min_g * min_r)
+}
+
+fn check_game_valid(game: &Vec<Vec<Pair>>) -> bool {
     const MAX_R: Pair = Pair {
         value: 12,
         color: Colors::Red,
@@ -49,39 +109,23 @@ fn main() {
         color: Colors::Blue,
     };
 
-    let games: Vec<Vec<Vec<Pair>>> = fs::read_to_string("input.txt")
-        .unwrap()
-        .lines()
-        .map(|x| parse_line(x.to_lowercase().as_str()))
-        .collect();
+    let mut game_valid: bool = true;
+    for set in game {
+        for pair in set {
+            let valid: bool = match pair.color {
+                Colors::Red => pair.value <= MAX_R.value,
+                Colors::Green => pair.value <= MAX_G.value,
+                Colors::Blue => pair.value <= MAX_B.value,
+            };
 
-    let mut total: usize = 0;
-
-    for (i, game) in games.iter().enumerate() {
-        println!("Game {}", i + 1);
-
-        let mut game_valid: bool = true;
-        for set in game {
-            for pair in set {
-                let valid: bool = match pair.color {
-                    Colors::Red => pair.value <= MAX_R.value,
-                    Colors::Green => pair.value <= MAX_G.value,
-                    Colors::Blue => pair.value <= MAX_B.value,
-                };
-
-                if !valid {
-                    game_valid = false;
-                    break;
-                }
+            if !valid {
+                game_valid = false;
+                break;
             }
-        }
-
-        if game_valid {
-            total += (i + 1);
         }
     }
 
-    println!("Total: {}", total);
+    game_valid
 }
 
 fn parse_line(line: &str) -> Vec<Vec<Pair>> {
