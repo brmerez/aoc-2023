c86242bcc6021b561a6b319555b5a8ed8656c346
Bruno Merez <brmerez@gmail.com>
2023-12-14 19:27:07 -0300

    Dia 4 -- parte 1

---
+++ 4/day-four/src/main.rs
@@ -0,0 +1,57 @@
+use std::fs;
+
+fn main() {
+    let file = fs::read_to_string("input.txt").unwrap();
+    let cards: Vec<Card> = file.lines().map(strip_line).collect();
+    let total = cards.iter().enumerate().fold(0, |acc, (index, card)| {
+        let points = check_winners(card);
+        println!("Card {index}: {points} points");
+        acc + points
+    });
+
+    println!("Total de pontos: {total}");
+}
+
+fn check_winners(card: &Card) -> i32 {
+    let points = card.played_numbers.iter().fold(0, |acc, played_num| {
+        // Provavelmente há um jeito de fazer isso mais rápido
+        // fazendo parse dos números, dando sort no vetor e usando binary search
+        if card.winning_numbers.contains(played_num) {
+            if acc == 0 {
+                return acc + 1;
+            }
+            return acc * 2;
+        }
+        acc
+    });
+
+    points
+}
+
+fn strip_line(line: &str) -> Card {
+    let mut result = line.split(" | ");
+    let winners = result.next().unwrap();
+    let played = result.next().unwrap();
+
+    let winning_numbers: Vec<String> = winners
+        .split(" ")
+        .filter(|num| !num.trim().is_empty())
+        .map(|num| num.trim().to_owned())
+        .collect();
+
+    let played_numbers: Vec<String> = played
+        .split(" ")
+        .filter(|num| !num.trim().is_empty())
+        .map(|num| num.trim().to_owned())
+        .collect();
+
+    Card {
+        winning_numbers,
+        played_numbers,
+    }
+}
+
+struct Card {
+    winning_numbers: Vec<String>,
+    played_numbers: Vec<String>,
+}
